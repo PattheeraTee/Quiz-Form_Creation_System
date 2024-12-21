ed6e799d834a428a70a3db730df2d98d4eadf82c
Pattheera-Teerarujinont <pattheera.t@kkumail.com>
2024-12-22 06:52:39 +0700

    edit format.json and change route /

--- Backend_QuizCreation/src/routes/formRoutes.js
+++ Backend_QuizCreation/src/routes/formRoutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const formController = require('../controller/formController');
 
 // เส้นทางสำหรับดึงข้อมูลแบบฟอร์ม
-router.get('form/:formId', formController.getForm);
+router.get('/form/:formId', formController.getForm);
 
 // เส้นทางสำหรับสร้างแบบฟอร์มใหม่
-router.post('form/create', formController.createForm);
+router.post('/form/create', formController.createForm);
 
 module.exports = router;

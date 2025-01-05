a5ca0020ce2c4f8425ad4c1b6f9cba37de003960
Pattheera-Teerarujinont <pattheera.t@kkumail.com>
2025-01-05 13:32:36 +0700

    change from main

---
+++ Backend_QuizCreation/src/controller/responseController.js
@@ -0,0 +1,46 @@
+const responseService = require("../service/responseService");
+const formRepository = require('../repository/formRepository');
+
+exports.submitResponse = async (req, res) => {
+  try {
+    const { form_id } = req.body;
+
+    // ดึงประเภทของ Form
+    const form = await formRepository.getForm(form_id);
+
+    if (!form) {
+      return res.status(404).json({ error: "Form not found" });
+    }
+
+    // เรียกใช้ Service เพื่อบันทึก Response และคำนวณคะแนน
+    const response = await responseService.submitQuizResponse(
+      req.body,
+      form.form_type
+    );
+
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.getResponsesByForm = async (req, res) => {
+  const { formId } = req.params;
+
+  try {
+    const responses = await responseService.getResponsesByForm(formId);
+    res.status(200).json(responses);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.deleteResponses = async (req, res) => {
+  try {
+    const { responseIds } = req.body; // Expecting an array of response_ids
+    const result = await responseService.deleteResponses(responseIds);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};

064b969329c81c04b02d73c3d24dcb2ed4f738e1
Phetchapa <phetchapa.n@kkumail.com>
2025-03-06 21:57:46 +0700

    add generateFormRouter

--- Backend_QuizCreation/index.js
+++ Backend_QuizCreation/index.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 const userRouter = require("./src/routes/userRoute");
 const formRouter = require("./src/routes/formRoutes");
 const responseRouter = require("./src/routes/responseRoutes");
+const generateFormRouter = require("./src/routes/geminiRoutes");
 const port = 3001;
 
 mongoose
@@ -29,4 +30,5 @@ app.use(cookieParser());
 app.use(express.json());
 app.use("/form", formRouter);
 app.use("/response", responseRouter);
+app.use("/", generateFormRouter);
 app.use("/",userRouter);

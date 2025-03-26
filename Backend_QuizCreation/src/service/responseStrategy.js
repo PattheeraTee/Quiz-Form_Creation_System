92c5985b287c7d7b1ab96470bbe23774ba3c70c3
Phetchapa <phetchapa.n@kkumail.com>
2025-03-27 02:52:37 +0700

    add Strategy Pattern detailResponseStrategies

--- Backend_QuizCreation/src/service/responseStrategy.js
+++ Backend_QuizCreation/src/service/responseStrategy.js
@@ -1,5 +1,6 @@
 const responseService = require("./responseService");
 
+// ใช้ Strategy Pattern เพื่อเลือกวิธีดึงข้อมูลในหน้าการตอบกลับ
 const responseStrategies = {
     quiz: responseService.getResponsesByQuizForm,
     survey: responseService.getResponsesBySurveyForm,
@@ -10,6 +11,22 @@ exports.getResponsesByType = async (formType, formId) => {
 
     if (!strategy) {
     throw new Error(`No response strategy found for form type: ${formType}`);
+    }
+
+    return await strategy(formId);
+};
+
+// ใช้ Strategy Pattern เพื่อเลือกวิธีดึงข้อมูลในตารางหน้าการตอบกลับ
+const detailResponseStrategies = {
+    quiz: responseService.getDetailResponsesByQuizForm,
+    survey: responseService.getDetailResponsesBySurveyForm,
+};
+
+exports.getDetailResponsesByType = async (formType, formId) => {
+    const strategy = detailResponseStrategies[formType];
+
+    if (!strategy) {
+        throw new Error(`No detail response strategy found for form type: ${formType}`);
     }
 
     return await strategy(formId);
